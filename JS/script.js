8deeec664edb8fb1cf93c82ee566f9edeed50381
Elena <77905432@mail.ru>
2022-05-08 15:18:02 +0300

    login form implementation

---
+++ JS/script.js
@@ -0,0 +1,78 @@
+"use strict"
+
+/*let arr = ["a", "b", "c", "d"];
+arr.splice(0, 4, "a + b"); 
+arr.splice(1, 0, " c + d");
+alert(arr);*/
+
+/*let arr = [2, 5, 3, 9];
+let result = +[0] * +[1] + +[2] * +[3];
+alert(result);*/
+
+/*let arr = [ [1, 2, 3], [4, 5, 6], [7,8,9] ];
+alert(arr[1][0]);*/
+
+/*let arr = {js:["jQuery", "Angular"], php: "hello", css: "world"};
+alert(arr["js"][0]);*/
+
+/*let arr = [];
+let str = '';
+for (let i=0; i < 10; i++) {
+str += 'x';
+arr.push(str);
+}
+alert(arr);*/
+
+/*let arr = [];
+let str;
+for (let i=1; i < 10; i++) {
+str = '';
+for (let j=1; j <= i; j++)
+str += i;
+arr.push(str);
+}
+alert(arr);*/
+
+/*function arrayFill(a,b) {
+    let arr = [];
+    for (let i=0; i<b; i++) {
+    arr.push(a);
+    }
+    return arr;
+    }
+    alert(arrayFill('x', 6));*/
+
+/*let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let sum = 0;
+for (let i=0; i < arr.length; i++) {
+sum += arr[i];
+if (sum > 10) {
+break;
+}
+}
+alert(i + 1);   */
+
+
+/*function sum(a) {
+    let b=0;
+    for (let i=0; i<a.length; i++) {
+    for (let j=0; j<a[i].length; j++){
+    b += a[i][j];
+    }
+    }
+    return b;
+    }
+    alert(sum([[1, 2, 3], [4, 5], [6]]));*/
+
+    /*function sum(a) {
+        b=0;
+        for (let i=0; i<a.length; i++) {
+        for (let j=0; j<a[i].length; j++){
+        for (let k=0; k<a[i][j].length; k++){
+        b += a[i][j][k];
+        }
+        }
+        }
+        return b;
+        }
+        alert(sum([[[1, 2], [3, 4]], [[5, 6], [7, 8]]]));*/ 
